4b8e4020a0ebc0f29909f1b59a12e04e8b3f9557
dongchangYoo <yoodc8167@gmail.com>
2024-03-28 13:47:42 +0900

    feat: add `n` option to setup.rs
    User can generate `params` about `n` specified from command line.
    If the `n` option is omitted, `setup` generates 2 params about `DEGREE`
    and `AGG_DEGREE`.

--- bin/src/setup.rs
+++ bin/src/setup.rs
@@ -1,5 +1,8 @@
 use clap::Parser;
-use zkevm::{circuit::DEGREE, utils::load_or_create_params};
+use zkevm::{
+    circuit::{AGG_DEGREE, DEGREE},
+    utils::create_kzg_params_to_file,
+};
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
@@ -7,6 +10,9 @@ struct Args {
     /// Specify directory which params have stored in. (default: ./kzg_params)
     #[clap(default_value = "./kzg_params", short, long)]
     params_dir: String,
+    /// Specify domain size. (generate 2 params with `DEGREE`/`AGG_DEGREE` if it is omitted.)
+    #[clap(default_value_t = 0, short)]
+    n: usize,
 }
 
 fn main() {
@@ -14,5 +20,13 @@ fn main() {
     env_logger::init();
 
     let args = Args::parse();
-    load_or_create_params(&path, *DEGREE).expect("failed to load or create params");
+    let params_dir = &args.params_dir;
+    if args.n == 0 {
+        let _ = create_kzg_params_to_file(params_dir, *DEGREE);
+        let _ = create_kzg_params_to_file(params_dir, *AGG_DEGREE);
+    } else if args.n > 30 {
+        panic!("too big domain size, you should enter `n` less than 30");
+    } else {
+        let _ = create_kzg_params_to_file(params_dir, args.n);
+    }
 }
